f8034be8711b823f9ce2d9c57f9e5a08db376b34
suganya s <Ssuganya974@gmail.com>
2026-03-23 12:08:26 +0530

    Remove unnecessary comment from script

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -4,7 +4,6 @@
 # Version: v1
 #
 #
-#
 # This script will help users to communicate and retrieve information from GitHub
 # Usage:
 #   Please provide your github token and rest api to the script as input
